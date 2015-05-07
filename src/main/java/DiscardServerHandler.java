ad749df9275eedccc78d7672c15427d5201c244a
Alex <alexndr86@gmail.com>
2015-05-07 23:09:32 +0300

    Echo server

--- src/main/java/DiscardServerHandler.java
+++ src/main/java/DiscardServerHandler.java
@@ -3,6 +3,7 @@ import io.netty.buffer.ByteBuf;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 
 /**
@@ -12,9 +13,19 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    //        ByteBuf in = (ByteBuf) msg;
+    //        try {
+    ////            while (in.isReadable()) { // (1)
+    ////                System.out.print((char) in.readByte());
+    ////                System.out.flush();
+    ////            }
+    //            System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+    //        } finally {
+    //            ReferenceCountUtil.release(msg); // (2)
+    //        }
     }
 
     @Override
